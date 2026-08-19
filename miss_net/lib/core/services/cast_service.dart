10c59b670eba40875fe7cd8bf4648d79b310a43e
panyou1996 <panyou1996@users.noreply.github.com>
2026-08-19 15:54:30 +0800

    chore: purge legacy flutter stack, keep pure kotlin native compose project

--- miss_net/lib/core/services/cast_service.dart
+++
@@ -1,9 +0,0 @@
-class CastService {
-  Future<void> init() async {}
-  
-  List<dynamic> getDevices() => [];
-
-  Future<void> cast(dynamic device, String url, String title) async {}
-  
-  void stop() {}
-}
